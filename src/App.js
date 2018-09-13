422fea3dd5ca8444c7678247796b9c4a45d5d6de
peaw1998 <m.peaw5500@gmail.com>
2018-09-13 11:09:18 +0700

    edit App3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import WordCard from './WordCard';
 import CharacterCard from './CharactorCard';
+
+
+const word = "PEAW";
 export default class App extends Component {
- render() {
- return (
- <div>
- <CharacterCard value="h"/>
-<CharacterCard value="i"/>
-{
-  Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+  render() {
+    return (
+      <div className="App">
+        <CharacterCard value="h" />
+        <CharacterCard value="i" />
+        <WordCard value="hello"/>
+        {
+          Array.from(word).map((c, i) => <CharacterCard value={c} key={i} />)
+        }
+      </div>
+    );
   }
- </div>
- );
- }
 }
